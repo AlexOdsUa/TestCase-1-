c2dd4402851e9166f5ee4ede6ecb283c0ad24c04
Oleksii <flotcka@gmail.com>
2020-02-12 11:38:27 +0200

    Added feature 'Remaining days to new Year 2021'

--- HelloWorldForJenkins.java
+++ HelloWorldForJenkins.java
@@ -1,12 +1,18 @@
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class HelloWorldForJenkins
 {
     public static void main(String[] args) {
         System.out.println("Hello world from Jenkins");
         System.out.println("Current time is: " + curTime());
+        daysToNewYear();
+
+
+
 
     }
 
@@ -15,4 +21,21 @@ public class HelloWorldForJenkins
         Date dateobj = new Date();
         return df.format(dateobj);
     }
+
+    public static void daysToNewYear(){
+
+        SimpleDateFormat myFormat = new SimpleDateFormat("dd MM yyyy");
+        String inputString1 = myFormat.format(new Date());
+        String inputString2 = "01 01 2021";
+
+        try {
+            Date date1 = myFormat.parse(inputString1);
+            Date date2 = myFormat.parse(inputString2);
+            long diff = date2.getTime() - date1.getTime();
+            System.out.println ("Days to new year is: " + TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
